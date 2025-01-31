fe5fe3a60d4f24f26097d8c2c85a641b5efc0009
Rashmi Ranjan Nayak <rrnayak165@gmail.com>
2025-01-31 11:16:51 +0530

    refactor : api name change from singular to plural

--- src/main/java/com/mindfire/ems/controller/DepartmentController.java
+++ src/main/java/com/mindfire/ems/controller/DepartmentController.java
@@ -27,35 +27,35 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
 
-    @PostMapping("/department")
+    @PostMapping("/departments")
     public ResponseEntity<DepartmentResponseDto> createDepartment(@Valid @RequestBody DepartmentRequestDto dto) {
         DepartmentResponseDto department = departmentService.addDepartment(dto);
 
         return new ResponseEntity<DepartmentResponseDto>(department, HttpStatus.CREATED);
     }
 
-    @GetMapping("/department")
+    @GetMapping("/departments")
     public ResponseEntity<List<DepartmentResponseDto>> getAll() {
         List<DepartmentResponseDto> departments = departmentService.getDepartments();
 
         return ResponseEntity.ok(departments);
     }
 
-    @GetMapping("/department/{id}")
+    @GetMapping("/departments/{id}")
     public ResponseEntity<DepartmentResponseDto> getDepartmentById(@PathVariable int id) {
         DepartmentResponseDto department = departmentService.getDepartment(id);
 
         return ResponseEntity.ok(department);
     }
 
-    @DeleteMapping("/department/{id}")
+    @DeleteMapping("/departments/{id}")
     public ResponseEntity<Void> removeDepartment(@PathVariable int id) {
         departmentService.deleteDepartment(id);
 
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/department/{id}")
+    @PutMapping("/departments/{id}")
     public ResponseEntity<DepartmentResponseDto> updateDepartment(@PathVariable int id,
             @RequestBody DepartmentRequestDto dto) {
         DepartmentResponseDto updatedDepartment = departmentService.updateDepartment(id, dto);
@@ -63,7 +63,7 @@ public class DepartmentController {
         return ResponseEntity.ok(updatedDepartment);
     }
 
-    @GetMapping("/department/employee-count")
+    @GetMapping("/departments/employee-count")
     public ResponseEntity<List<Object[]>> countEmployeePerDepartment() {
         List<Object[]> list = departmentService.employeePerDepartment();
         return ResponseEntity.ok(list);
